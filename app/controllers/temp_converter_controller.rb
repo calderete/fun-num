8dd6355068b38f7bd8d5bcc9b15558baf8f44299
Charles <codprestige10@gmail.com>
2016-02-20 22:00:12 -0500

    f and c temp conversions working as well as to kelvin

--- app/controllers/temp_converter_controller.rb
+++ app/controllers/temp_converter_controller.rb
@@ -1,18 +1,42 @@
 class TempConverterController < ApplicationController
 
 	def f_to_c
-		
-		temp = params[:temp].to_f
-		scale = params[:scale]
-		
+		temp, scale = params[:temp].to_f, params[:scale].downcase
 		case scale
-		when "C", "c"
+		when "c"
 			f = 1.8 * temp + 32
-		when "F", "f"
+			render json: {message: "#{f} degrees C"}, status: 200
+		when "f"
 			c = (5.0/9.0)*(temp - 32)
+			render json: {message: "#{c} degrees F"}, status: 200
 		else 
-			abort "Must be C or F"
+			render json: {message: "#{temp}, or #{scale} is not valid"},
+										status: 400 
 		end
-		
+	end
+
+	def to_k
+		temp, scale = params[:temp].to_f, params[:scale].downcase
+		case scale
+			when "f"
+				f2k = (temp - 32) * 5.0/9.0 + 273.15
+				render json: { meassage:"#{f2k} degrees Kelvin"}, status: 200
+			when "c"
+				c2k = temp + 273.15
+				render json: {message: "#{c2k} degrees Kelvin"}, status: 200
+			else
+				render json: {message: "#{temp}, or #{scale} is not valid"},
+										status: 400 
+		end
 	end
 end
+
+
+
+
+
+
+
+
+
+
