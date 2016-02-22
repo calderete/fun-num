30d8c844a366b0c80a90f8b3818a80f4d92bfeb2
Charles <codprestige10@gmail.com>
2016-02-21 19:48:46 -0500

    fixed some bugs so the heroku server would not crash again

--- app/controllers/distance_converter_controller.rb
+++ app/controllers/distance_converter_controller.rb
@@ -5,60 +5,72 @@ class DistanceConverterController < ApplicationController
 	end
 
 	def from_lightyears
-		distance, scale = params[:distance].to_f, params[:scale].downcase
-		case scale
-			when "m"
-				d = distance * 5865696e6
-				@answer = "#{distance} lightyears is " + d.to_s + "Miles"
-				render :answer
-			when "k"
-				d = distance * 94608e8
-				@answer = "#{distance.to_i} lightyears is " + d.to_s + "Kilometers"
-				render :answer
-			else
-				render json: {message: "#{scale} is invalid go back and enter 'm' or 'k'"},
+		unless params[:distance].to_f == 0
+			distance, scale = params[:distance].to_f, params[:scale].downcase
+			case scale
+				when "m"
+					d = distance * 5865696e6
+					@answer = "#{distance} lightyears is " + d.to_s + "Miles"
+					render :answer
+				when "k"
+					d = distance * 94608e8
+					@answer = "#{distance.to_i} lightyears is " + d.to_s + "Kilometers"
+					render :answer
+				else
+					render json: {message: "#{scale} is invalid go back and enter 'm' or 'k'"},
 											status: 400
+			end
+		else
+			render json: {message: "invalid distance"}
 		end
 	end
 
 	def from_au
-		distance, scale = params[:distance].to_f, params[:scale].downcase
-		case scale
-			when "m"
-				d = distance * 9296e7
-				@answer = "#{distance} astronimcal units is" + d.to_s + "Miles"
-				render :answer
-			when "k"
-				d = distance * 1496e8
-				@answer = "#{distance} astronimcal units is " + d.to_s + "Kilometers"
-			else
-				render json: {message: "#{scale} is invalid. Go back and enter 'm' or 'k'"},
+		unless params[:distance].to_f == 0
+			distance, scale = params[:distance].to_f, params[:scale].downcase
+			case scale
+				when "m"
+					d = distance * 9296e7
+					@answer = "#{distance} astronimcal units is" + d.to_s + "Miles"
+					render :answer
+				when "k"
+					d = distance * 1496e8
+					@answer = "#{distance} astronimcal units is " + d.to_s + "Kilometers"
+				else
+					render json: {message: "#{scale} is invalid. Go back and enter 'm' or 'k'"},
 											status: 400
-			end
+				end
+		else
+			render json: {message: "invalid distance"}
+		end
 	end
 
 	def from_parsec
-		distance, scale = params[:distance].to_f, params[:scale].downcase
-		case scale 
-		when "m"
-			d = distance * 1.9174e13
-			@answer = "#{distance} parsecs is " + d.to_s + "Miles"
-			render :answer
-		when "k" 
-			d = distance * 3.086e13
-			@answer = "#{distance} parsecs is " + d.to_s + "Kilometers"
-			render :answer
-		when "ly"
-			d = distance * 3.2656
-			@answer = "#{distance} parsecs is " + d.to_s + "Lightyears"
-			render :answer
-		when "au"
-			d = distance * 2.0626e5
-			@answer = "#{distance} parsecs is " + d.to_s + "AU"
-			render :answer
-		else
-			render json: {message: "#{scale} is invalid. Go back and enter 'm' or 'k'"},
+		unless params[:distance].to_f == 0
+			distance, scale = params[:distance].to_f, params[:scale].downcase
+			case scale 
+			when "m"
+				d = distance * 1.9174e13
+				@answer = "#{distance} parsecs is " + d.to_s + "Miles"
+				render :answer
+			when "k" 
+				d = distance * 3.086e13
+				@answer = "#{distance} parsecs is " + d.to_s + "Kilometers"
+				render :answer
+			when "ly"
+				d = distance * 3.2656
+				@answer = "#{distance} parsecs is " + d.to_s + "Lightyears"
+				render :answer
+			when "au"
+				d = distance * 2.0626e5
+				@answer = "#{distance} parsecs is " + d.to_s + "AU"
+				render :answer
+			else
+				render json: {message: "#{scale} is invalid. Go back and enter 'm' or 'k'"},
 										status: 400
+			end
+		else
+			render json: {message: "invalid distance"}
 		end			
 	end
 
--- app/controllers/temp_converter_controller.rb
+++ app/controllers/temp_converter_controller.rb
@@ -5,49 +5,61 @@ class TempConverterController < ApplicationController
 	end
 
 	def f_to_c
-		temp, scale = params[:temp].to_f, params[:scale].downcase
-		case scale
-		when "c"
-			t, s = 1.8 * temp + 32, "Farenhiet"
-			show_answer(temp, scale, t, s)
-		when "f"
-			t,s = (5.0/9.0)*(temp - 32), "Celsius"
-			show_answer(temp, scale, t, s)
-		else 
-			render json: {message: "#{scale} is invalid go back and enter 'f' or 'c'"},
+		unless params[:temp].to_f == 0
+			temp, scale = params[:temp].to_f, params[:scale].downcase
+			case scale
+			when "c"
+				t, s = 1.8 * temp + 32, "Farenhiet"
+				show_answer(temp, scale, t, s)
+			when "f"
+				t,s = (5.0/9.0)*(temp - 32), "Celsius"
+				show_answer(temp, scale, t, s)
+			else 
+				render json: {message: "#{scale} is invalid go back and enter 'f' or 'c'"},
 										status: 400 
+			end
+		else
+			render json: {message: "invalid temp"}
 		end
 	end
 
 	def to_k
-		temp, scale = params[:temp].to_f, params[:scale].downcase
-		case scale
-			when "f"
-				t, s = (temp - 32) * 5.0/9.0 + 273.15, "Kelvin"
-				show_answer(temp, scale, t, s)
-			when "c"
-				t, s = temp + 273.15, "Kelvin"
-				show_answer(temp, scale, t, s)
-			else
-				render json: {message: "#{scale} is invalid go back and enter 'f' or 'c'"},
+		unless params[:temp].to_f == 0
+			temp, scale = params[:temp].to_f, params[:scale].downcase
+			case scale
+				when "f"
+					t, s = (temp - 32) * 5.0/9.0 + 273.15, "Kelvin"
+					show_answer(temp, scale, t, s)
+				when "c"
+					t, s = temp + 273.15, "Kelvin"
+					show_answer(temp, scale, t, s)
+				else
+					render json: {message: "#{scale} is invalid go back and enter 'f' or 'c'"},
 										status: 400 
+			end
+		else
+			render json: {message: "invalid temp"}
 		end
 	end
 
 	def from_k
-		temp, scale = params[:temp].to_f, params[:scale].downcase
-		case scale
-			when "f"
-				t = temp - 273.15 * 9.0/5.0 + 32
-				@answer = "#{temp} degrees Kelvin =" + t.round(3).to_s + "#{scale.upcase}"
-				render :temp_answer
-			when "c"
-				t = temp - 273.15
-				@answer = "#{temp} degrees Kelvin =" + t.round(3).to_s + "#{scale.upcase}"
-				render :temp_answer
-			else
-				render json: {message: "##{scale} is invalid go back and enter 'f' or 'c'"},
+		unless params[:temp].to_f == 0
+			temp, scale = params[:temp].to_f, params[:scale].downcase
+			case scale
+				when "f"
+					t = temp - 273.15 * 9.0/5.0 + 32
+					@answer = "#{temp} degrees Kelvin =" + t.round(3).to_s + "#{scale.upcase}"
+					render :temp_answer
+				when "c"
+					t = temp - 273.15
+					@answer = "#{temp} degrees Kelvin =" + t.round(3).to_s + "#{scale.upcase}"
+					render :temp_answer
+				else
+					render json: {message: "#{scale} is invalid go back and enter 'f' or 'c'"},
 										status: 400 
+			end
+		else
+			render json: {message: "invalid temp"}
 		end
 	end
 
