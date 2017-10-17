30a20553a4ae2802bbe3ba1bb12a66a0afffb04a
Charles <codprestige10@gmail.com>
2017-10-17 11:47:27 -0400

    started pi tracker app

---
+++ app/controllers/eve_pi_home.rb
@@ -0,0 +1,5 @@
+class EvePiHome < ApplicationController
+  def menu
+   render :eve_pi_menu
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -25,10 +25,12 @@ post 'conversion/distance/from_ly', to: 'distance_converter#from_lightyears', as
 post 'conversion/distance/from_au', to: 'distance_converter#from_au', as: 'from_au'
 post 'conversion/distance/from_parsec', to: 'distance_converter#from_parsec', as: 'from_parsec'
 
-get  'test/number', to: 'number_test#test_selector', as: 'number_test'  
+get  'test/number', to: 'number_test#test_selector', as: 'number_test'
 post 'test/happy_number', to: 'number_test#happy_test', as: 'happy_test'
-  
+
 post 'test/levenshtein', to: 'levenshtein#compare', as: 'levenshtein_test'
+
+get 'eve/pi/home', to: 'eve_pi_home#menu', as: 'eve_pi'
   #   resources :products dos
   #     member do
   #       get 'short'
